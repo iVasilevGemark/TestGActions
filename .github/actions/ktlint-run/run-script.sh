62442bbc098102e85738e8f9daffa2c1c6d6fc2c
Igor Vasilev <i.vasilev@gemark.ru>
2021-03-21 13:08:19 +0300

    add android lin

--- .github/actions/ktlint-run/run-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -o pipefail
-
-echo Using KtLint version: "$(ktlint --version)"
-
-ktlint --android "**/src/**/*.kt" --reporter=plain --reporter=checkstyle,output=ktlint-report-in-checkstyle-format.xml
