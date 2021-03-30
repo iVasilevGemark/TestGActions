bbbbc0cd1b8935bcee33ba98bad820daf2d5cc15
Igor Vasilev <i.vasilev@gemark.ru>
2021-03-30 10:26:10 +0300

    Generation

---
+++ buildSrc/src/main/kotlin/constants/Android.kt
@@ -0,0 +1,15 @@
+package constants
+
+object Android {
+    const val APPLICATION_ID = "ru.inventos.apps.tv1000play"
+    const val BUILD_TOOLS_VERSION = "30.0.3"
+
+    const val MIN_SDK_VERSION = 22
+    const val TARGET_SDK_VERSION = 30
+    const val COMPILE_SDK_VERSION = 30
+
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "0.1"
+
+    const val ANDROID_TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
+}
